2dc01585c7b313d962aff110575e8b1115c912cd
Jean Johnsen <Geniusnh4@gmail.com>
2019-10-31 13:26:50 +0100

    encodeFile() implemented in the code

--- EdgeDetec/src/semester/projekt/starter/Starter.java
+++ EdgeDetec/src/semester/projekt/starter/Starter.java
@@ -6,7 +6,10 @@
 package semester.projekt.starter;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Base64;
 import javax.imageio.ImageIO;
 import semester.projekt.core.EdgeDetector;
 
@@ -14,19 +17,32 @@ import semester.projekt.core.EdgeDetector;
  *
  * @author geniu
  */
-public class Starter {
-        public static void main(String[] args) throws IOException {
+public class Starter 
+{
+        public static void main(String[] args) throws IOException, Exception 
+        {
         System.out.println("Started");
         System.out.println("Enter the file path and name with file type :");
 
         semester.projekt.core.EdgeDetector con = new semester.projekt.core.EdgeDetector();
 
-        File outputfile = new File("sobel.png");
-        ImageIO.write(con.getBufferedImage(), "png", outputfile);
+        File file = new File("sobel.png");
+        ImageIO.write(con.getBufferedImage(), "png", file);
 
         System.out.println("max : " + con.getClass());
         System.out.println("Finished");
+        
+        String encodestring = encodeFile(file);
+        System.out.println(encodestring);
 
-    }
-    
+        }
+        
+        private static String encodeFile(File file) throws Exception
+        {
+        FileInputStream fileInputStreamReader = new FileInputStream(file);
+        byte[] bytes = Files.readAllBytes(file.toPath());
+        fileInputStreamReader.read(bytes);
+        
+        return new String(Base64.getEncoder().encode(bytes), "UTF-8");
+        }
 }
